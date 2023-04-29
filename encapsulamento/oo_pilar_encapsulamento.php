96b2680f2cf74136c803fdb3cf05a1d97c1d41fc
Daniel Andrade <danielandrade539@gmail.com>
2023-04-28 22:26:42 -0300

    encapsulamento

--- encapsulamento/oo_pilar_encapsulamento.php
+++ encapsulamento/oo_pilar_encapsulamento.php
@@ -4,12 +4,39 @@
         private $nome = 'Daniel';
         protected $sobrenome = 'Andrade';
         public $humor = 'Alegre';
-    }
 
-    $pai = new Pai();
-    echo $pai->humor;
-    $pai->humor = 'Triste';
-    echo '<hr>';
-    echo $pai->humor;
+        public function __get($attr) {
+			return $this->$attr;
+		}
+
+		
+		public function __set($attr, $value) {
+			$this->$attr = $value;
+		}
+
+		private function executarMania() {
+			echo 'Assobiar';
+		}
+
+		protected function responder() {
+			echo 'Oi';
+		}
+
+		public function executarAcao() {
+			$x = rand(1, 10);
+
+			if($x >= 1 && $x <= 8) {
+				$this->responder();	
+			} else {
+				$this->executarMania();	
+			}			
+		}
+		
+	}
+
+	$pai = new Pai();
+	//echo $pai->humor;
+	echo $pai->executarAcao();
+
 
 ?>
